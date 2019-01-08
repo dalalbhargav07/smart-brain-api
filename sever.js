5c5dff6275f62dd5ce2e70c5e029656eabcb015d
dalalbhargav07 <dalal.bhargav07@gmail.com>
2019-01-08 08:47:13 -0400

    Adding port

--- sever.js
+++ sever.js
@@ -85,6 +85,6 @@ app.post('/imageurl', (req, res) => {
 	
 // });
 
-app.listen(3001, () => {
-	console.log('App is running fine on port 3001')
+app.listen(process.env.PORT || 3001, () => {
+	console.log(`App is running fine on port ${process.env.PORT}`)
 })
