63f95a74ff51d22d18540824a4babb84fe9c696e
dalalbhargav07 <dalal.bhargav07@gmail.com>
2019-01-08 09:50:33 -0400

    fixing a bug

--- controller/register.js
+++ controller/register.js
@@ -2,6 +2,7 @@
 
 const handleRegister = (req, res, db, bcrypt) => {
 	const { email, name, password } = req.body
+	console.log('email')
 	if (!email || !name || !password) {
 		return res.status(400).json('incorrect form submission')
 	}
