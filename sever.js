0ce51fdc7da235133f03e0b9dfcfee22022a3a57
dalalbhargav07 <dalal.bhargav07@gmail.com>
2019-01-08 09:05:36 -0400

    Changing port

--- sever.js
+++ sever.js
@@ -57,6 +57,6 @@ app.post('/imageurl', (req, res) => {
 	
 // });
 
-app.listen(process.env.PORT || 3001, () => {
+app.listen(process.env.PORT || 3000, () => {
 	console.log(`App is running fine on port ${process.env.PORT}`)
 })
